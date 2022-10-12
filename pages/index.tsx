69650ce4aad8bc2ee26a1f69e243d79abb4e419e
Raymond Brown <daimyoplays@gmail.com>
2022-10-12 12:30:32 -0500

    edited styles and front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,17 @@ const Home: NextPage = ({ planets }: any) => {
             backgroundColor = "bg-amber-400";
             hoverColor = "hover:bg-amber-600";
           }
+          if (planet.climate == "temperate") {
+            backgroundColor = "bg-green-500";
+            hoverColor = "hover:bg-green-700";
+          }
+          if (planet.climate == "frozen") {
+            backgroundColor = "bg-blue-500";
+            hoverColor = "hover:bg-blue-700";
+          }
           return (
             <div
+              key={planet.name}
               className={`font-ubuntu ${
                 backgroundColor != "" ? backgroundColor : " bg-emerald-500"
               } ${
@@ -31,9 +40,15 @@ const Home: NextPage = ({ planets }: any) => {
               </h1>
               <h1
                 key={planet.population}
-                className="px-10 py-3 text-2xl text-white"
+                className="px-10 py-3 pb-2 text-2xl text-white font-ubuntu"
               >
                 Population: {planet.population}
+              </h1>
+              <h1
+                key={planet.climate}
+                className="px-10 py-3 text-2xl text-white capitalize font-ubuntu"
+              >
+                Climate: {planet.climate}
               </h1>
             </div>
           );
@@ -44,8 +59,15 @@ const Home: NextPage = ({ planets }: any) => {
 
   return (
     <div className="w-screen text-center bg-gradient-to-b from-orange-500 to-red-600 h-max">
-      <h1 className="text-8xl p-4 pt-40 pb-20 uppercase font-bold text-white font-gemunulibre">
+      <h1 className="text-8xl p-4 pt-40 pb-10 uppercase font-bold text-white font-gemunulibre">
         Welcome To SWInfo!
+      </h1>
+      <h1 className="text-3xl pb-10 text-white max-w-6xl mx-auto">
+        SWInfo is a small wikipedia for everything Star Wars. You can look at
+        the navigation at the top to see the different pages of content!
+      </h1>
+      <h1 className="text-6xl pb-3 text-white max-w-6xl mx-auto uppercase font-gemunulibre">
+        Planets
       </h1>
       <div>
         <h1>{getData()}</h1>
